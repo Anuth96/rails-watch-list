0cb3d1a4c240bfd437895f3dcb9d2568f8b42ac4
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-06-16 08:56:21 +1000

    font-awesome icon works

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create]
-  resources :lists do
+  resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
+
   resources :bookmarks, only: [:destroy]
+
+  resources :lists do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
