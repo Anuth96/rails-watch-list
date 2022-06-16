89d7c26359f01e1c50e81192365b36dd214ac6d7
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-06-16 10:31:26 +1000

    Modified Gemfile.lock

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 # config/application.rb
-config.assets.initialize_on_precompile = false
 module RailsWatchList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
